5c944a7a65a2f5f9e0f8991032aee8a5a5ddb0c1
Guoqing <zhaogq10@163.com>
2014-07-17 22:58:45 +0800

    [select destination by atlas id and print history] [Guoqing]

--- src/destination_xml_parser.rb
+++ src/destination_xml_parser.rb
@@ -10,22 +10,46 @@ class DestinationXMLParser
   end
 
   def parse
-    parseNode @taxonomy_root, 0
+    parse_node @taxonomy_root, 0
   end
 
-  def parseNode root, depth
+  def select_destination_by_atlas_id atlas_id
+    results = []
+    @destinations_root.xpath("destination").each do |child_node|
+      results << child_node if child_node['atlas_id'] == atlas_id
+    end
+    results
+  end
+
+  private
+  def parse_node node, depth
     depth += 1
-    if root['atlas_node_id']
-      print_atlas(depth, root)
-    end
-    root.children.each do |node|
-      parseNode node, depth
+    handle_atlas(depth, node) if node['atlas_node_id']
+    node.children.each do |child_node|
+      parse_node child_node, depth
     end
   end
 
-  def print_atlas(depth, root)
-    depth.times { print ' ' }
-    print "- #{root['atlas_node_id']} "
-    p "#{root.at_xpath('node_name').text}"
+  def handle_atlas(depth, node)
+    print_atlas(depth, node)
+
+    atlas_destinations = select_destination_by_atlas_id node['atlas_node_id']
+    atlas_destinations.each do |destination_node|
+      print_destination 0, destination_node
+    end
+  end
+
+  def print_destination(depth, node)
+    print_desgitnation_info node
+  end
+
+  def print_desgitnation_info node
+    p node.at_xpath('history').text if node.at_xpath('history')
+  end
+
+  def print_atlas(depth, node)
+    depth.times { print '  ' }
+    print "- #{node['atlas_node_id']} "
+    p "#{node.at_xpath('node_name').text}"
   end
 end
--- src/parse.rb
+++ src/parse.rb
@@ -3,3 +3,4 @@ require 'nokogiri'
 
 parser = DestinationXMLParser.new('../resource/taxonomy.xml', '../resource/destinations.xml')
 parser.parse
+parser.select_destination_by_atlas_id '355611'
