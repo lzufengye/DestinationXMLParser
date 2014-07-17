594386b4d08d9cbdc1bb04ffc130aeaf7a09a00c
Guoqing <zhaogq10@163.com>
2014-07-17 23:29:54 +0800

    [print destination] [Guoqing]

--- src/destination_xml_parser.rb
+++ src/destination_xml_parser.rb
@@ -13,14 +13,6 @@ class DestinationXMLParser
     parse_node @taxonomy_root, 0
   end
 
-  def select_destination_by_atlas_id atlas_id
-    results = []
-    @destinations_root.xpath("destination").each do |child_node|
-      results << child_node if child_node['atlas_id'] == atlas_id
-    end
-    results
-  end
-
   private
   def parse_node node, depth
     depth += 1
@@ -35,21 +27,77 @@ class DestinationXMLParser
 
     atlas_destinations = select_destination_by_atlas_id node['atlas_node_id']
     atlas_destinations.each do |destination_node|
-      print_destination 0, destination_node
+      print_destination depth, destination_node
     end
   end
 
   def print_destination(depth, node)
-    print_desgitnation_info node
+    print_tab depth+2
+    print '- history:'
+    print_tab depth+4
+    print_desgitnation_info node, 'history'
+    p ''
+
+    print_tab depth+2
+    print '- introductory'
+    p ''
+    print_tab depth+4
+    print '- introduction'
+    p ''
+    print_tab depth+6
+    print '- overview '
+    print_desgitnation_info node, 'overview'
+    p ''
+
+    print '- practical_information'
+      print '- health_and_safety'
+        print_desgitnation_info node, 'dangers_and_annoyances'
+        print_desgitnation_info node, 'while_youre_there'
+      print '- money_and_costs'
+        print_desgitnation_info node, 'costs'
+        print_desgitnation_info node, 'money'
+      print 'visas'
+        print_desgitnation_info node, 'overview'
+    print 'transport'
+      print 'getting_there_and_around'
+        print_desgitnation_info node, 'air'
+        print_desgitnation_info node, 'bicycle'
+        print_desgitnation_info node, 'bus_and_tram'
+        print_desgitnation_info node, 'car_and_motorcycle'
+        print_desgitnation_info node, 'local_transport'
+        print_desgitnation_info node, 'overview'
+        print_desgitnation_info node, 'train'
+    print 'weather'
+      print 'when_to_go'
+        print_desgitnation_info node, 'climate'
+    print 'work_live_study'
+      print 'work'
+        print_desgitnation_info node, 'business'
+        print_desgitnation_info node, 'overview'
+
+    p ''
   end
 
-  def print_desgitnation_info node
-    p node.at_xpath('history').text if node.at_xpath('history')
+  def print_desgitnation_info node, title
+    p node.at_xpath(title).text if node.at_xpath(title)
   end
 
   def print_atlas(depth, node)
     depth.times { print '  ' }
     print "- #{node['atlas_node_id']} "
     p "#{node.at_xpath('node_name').text}"
+  end
+
+
+  def select_destination_by_atlas_id atlas_id
+    results = []
+    @destinations_root.xpath("destination").each do |child_node|
+      results << child_node if child_node['atlas_id'] == atlas_id
+    end
+    results
+  end
+
+  def print_tab depth
+    depth.times { print '  ' }
   end
 end
--- src/parse.rb
+++ src/parse.rb
@@ -3,4 +3,3 @@ require 'nokogiri'
 
 parser = DestinationXMLParser.new('../resource/taxonomy.xml', '../resource/destinations.xml')
 parser.parse
-parser.select_destination_by_atlas_id '355611'
